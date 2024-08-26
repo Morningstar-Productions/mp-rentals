88d461af9e867d5e95678e5cad770584eeb13f78
CFOUR <116594943+cfoour@users.noreply.github.com>
2024-08-26 13:55:26 +0100

    Update client.lua

--- config/client.lua
+++ config/client.lua
@@ -1,21 +1,27 @@
 return {
-    fuelExport = 'LegacyFuel',
+    fuelExport = 'LegacyFuel', -- leave it empty to use ox_fuel or Renewed-Fuel
 
-    useTarget = false,
+    useTarget = false, -- false = interact | true = ox_target
     useOxQwade = false, -- Using Qwadebot's Variation of ox_lib using Search
 
     vehBlips = {
-        { title = locale("land_veh"), color = 50, id = 56, coords = vec3(109.55, -1089.71, 29.3)},
-        { title = locale("air_veh"), color = 32, id = 578, coords = vec3(-1673.39, -3158.45, 13.99)},
-        { title = locale("sea_veh"), color = 42, id = 410, coords = vec3(-753.55, -1512.24, 5.02)},
+        { title = locale("land_veh"), color = 50, id = 56,  coords = vec3(109.55, -1089.71, 29.3) },
+        { title = locale("air_veh"),  color = 32, id = 578, coords = vec3(-1673.39, -3158.45, 13.99) },
+        { title = locale("sea_veh"),  color = 42, id = 410, coords = vec3(-753.55, -1512.24, 5.02) },
     },
 
     vehSpawnLocations = {
-        vehicle = vec4(111.37, -1081.11, 29.19, 341.23),
+        vehicle = {
+            vec4(111.37, -1081.11, 29.19, 341.23),
+        },
 
-        aircraft = vec4(-1673.4, -3158.47, 13.99, 331.49),
+        aircraft = {
+            vec4(-1673.4, -3158.47, 13.99, 331.49),
+        },
 
-        boat = vec4(-794.95, -1506.27, 2.08, 107.79),
+        boat = {
+            vec4(-794.95, -1506.27, 2.08, 107.79),
+        },
     },
 
     rentalVehicles = {
@@ -97,7 +103,7 @@ return {
                 model = 'tourbus',
                 icon = 'fas fa-car',
                 money = 800,
-                --image = 'https://cdn.discordapp.com/attachments/907789876177555495/1065029457590620190/image.png', -- Image of the vehicle 
+                --image = 'https://cdn.discordapp.com/attachments/907789876177555495/1065029457590620190/image.png', -- Image of the vehicle
                 fuel = math.random(55, 77),
             },
             [11] = {
