9d5e1ae30961b53554158c980144048bb81b1df7
CFOUR <116594943+cfoour@users.noreply.github.com>
2024-08-26 13:52:58 +0100

    Update cl_functions.lua

--- client/cl_functions.lua
+++ client/cl_functions.lua
@@ -26,145 +26,152 @@ local function CreatePeds()
     local aircoords = pedInfo.aircraft
     local boatcoords = pedInfo.boat
 
-    Renewed.addPed({
-        model = `a_m_y_business_03`, -- The ped to be spawned
-        dist = 25, -- Distance of when the ped should be loaded in
-        coords = vec3(landcoords.x, landcoords.y, landcoords.z), -- The coords for the ped
-        heading = landcoords.w, -- Heading of the ped
-        scenario = 'WORLD_HUMAN_CLIPBOARD', -- Use this if you want the ped to play a scenario
-        freeze = true, -- use this if the ped should be frozen
-        invincible = true, -- use this if the ped should be invincible 
-        tempevents = true,-- use this if the ped should block temporary events
-        id = 'rentals_land_vehicles', -- Unique ID for the ped
+    for i = 1, #landcoords do
+        Renewed.addPed({
+            model = `a_m_y_business_03`,                                      -- The ped to be spawned
+            dist = 25,                                                        -- Distance of when the ped should be loaded in
+            coords = vec3(landcoords[i].x, landcoords[i].y, landcoords[i].z), -- The coords for the ped
+            heading = landcoords[i].w,                                        -- Heading of the ped
+            scenario = 'WORLD_HUMAN_CLIPBOARD',                               -- Use this if you want the ped to play a scenario
+            freeze = true,                                                    -- use this if the ped should be frozen
+            invincible = true,                                                -- use this if the ped should be invincible
+            tempevents = true,                                                -- use this if the ped should block temporary events
+            id = 'rentals_land_vehicles',                                     -- Unique ID for the ped
 
-        -- Normal ox Target stuff --
-        target = {
-            {
-                name = 'rentals_land_vehicles',
-                icon = "fas fa-car",
-                label = "Rent Vehicle",
-                onSelect = function()
-                    local data = { licenseType = 'driver', menuType = 'vehicle' }
-                    checkLicenseData(data)
-                end,
-                canInteract = function(_, distance)
-                    return distance < 2.5 and config.useTarget
-                end,
-            },
-        },
-
-        interact = {
-            distance = 3.0,
-            interactDst = 1.5,
-            options = {
+            -- Normal ox Target stuff --
+            target = {
                 {
-                    label = 'Rent Vehicle',
-                    action = function()
+                    name = 'rentals_land_vehicles',
+                    icon = "fas fa-car",
+                    label = "Rent Vehicle",
+                    onSelect = function()
                         local data = { licenseType = 'driver', menuType = 'vehicle' }
                         checkLicenseData(data)
                     end,
-                    canInteract = function()
-                        return not config.useTarget
-                    end
+                    canInteract = function(_, distance)
+                        return distance < 2.5 and config.useTarget
+                    end,
+                },
+            },
+
+            interact = {
+                distance = 3.0,
+                interactDst = 1.0,
+                options = {
+                    {
+                        label = 'Rent Vehicle',
+                        action = function()
+                            local data = { licenseType = 'driver', menuType = 'vehicle' }
+                            checkLicenseData(data)
+                        end,
+                        canInteract = function()
+                            return not config.useTarget
+                        end
+                    }
                 }
             }
-        }
-    })
+        })
+    end
 
-    Renewed.addPed({
-        model = `s_m_y_airworker`, -- The ped to be spawned
-        dist = 25, -- Distance of when the ped should be loaded in
-        coords = vec3(aircoords.x, aircoords.y, aircoords.z), -- The coords for the ped
-        heading = aircoords.w, -- Heading of the ped
-        scenario = 'WORLD_HUMAN_CLIPBOARD', -- Use this if you want the ped to play a scenario
-        freeze = true, -- use this if the ped should be frozen
-        invincible = true, -- use this if the ped should be invincible 
-        tempevents = true,-- use this if the ped should block temporary events
-        id = 'rentals_air_vehicles', -- Unique ID for the ped
+    for i = 1, #aircoords do
+        Renewed.addPed({
+            model = `s_m_y_airworker`,                                     -- The ped to be spawned
+            dist = 25,                                                     -- Distance of when the ped should be loaded in
+            coords = vec3(aircoords[i].x, aircoords[i].y, aircoords[i].z), -- The coords for the ped
+            heading = aircoords[i].w,                                      -- Heading of the ped
+            scenario = 'WORLD_HUMAN_CLIPBOARD',                            -- Use this if you want the ped to play a scenario
+            freeze = true,                                                 -- use this if the ped should be frozen
+            invincible = true,                                             -- use this if the ped should be invincible
+            tempevents = true,                                             -- use this if the ped should block temporary events
+            id = 'rentals_air_vehicles',                                   -- Unique ID for the ped
 
-        -- Normal ox Target stuff --
-        target = {
-            {
-                name = 'rentals_air_vehicles',
-                icon = "fas fa-car",
-                label = "Rent Vehicle",
-                onSelect = function()
-                    local data = { licenseType = 'pilot', menuType = 'aircraft' }
-                    checkLicenseData(data)
-                end,
-                canInteract = function(_, distance)
-                    return distance < 2.5 and config.useTarget
-                end,
-            },
-        },
-
-        interact = {
-            distance = 3.0,
-            interactDst = 1.5,
-            options = {
+            -- Normal ox Target stuff --
+            target = {
                 {
-                    label = 'Rent Vehicle',
-                    action = function()
+                    name = 'rentals_air_vehicles',
+                    icon = "fas fa-car",
+                    label = "Rent Vehicle",
+                    onSelect = function()
                         local data = { licenseType = 'pilot', menuType = 'aircraft' }
                         checkLicenseData(data)
                     end,
-                    canInteract = function()
-                        return not config.useTarget
-                    end
-                }
-            }
-        }
-    })
-
-    Renewed.addPed({
-        model = `mp_m_boatstaff_01`, -- The ped to be spawned
-        dist = 25, -- Distance of when the ped should be loaded in
-        coords = vec3(boatcoords.x, boatcoords.y, boatcoords.z), -- The coords for the ped
-        heading = boatcoords.w, -- Heading of the ped
-        scenario = 'WORLD_HUMAN_CLIPBOARD', -- Use this if you want the ped to play a scenario
-        freeze = true, -- use this if the ped should be frozen
-        invincible = true, -- use this if the ped should be invincible 
-        tempevents = true,-- use this if the ped should block temporary events
-        id = 'rentals_boat_vehicles', -- Unique ID for the ped
-
-        -- Normal ox Target stuff --
-        target = {
-            {
-                name = 'rentals_air_vehicles',
-                icon = "fas fa-car",
-                label = "Rent Vehicle",
-                onSelect = function()
-                    local data = { menuType = 'boat' }
-                    MPc.openVehicleMenu(data)
-                end,
-                canInteract = function(_, distance)
-                    return distance < 2.5 and config.useTarget
-                end,
-            },
-        },
-
-        interact = {
-            distance = 3.0,
-            interactDst = 1.5,
-            options = {
-                {
-                    label = 'Rent Vehicle',
-                    action = function()
-                        local data = { menuType = 'boat' }
-                        checkLicenseData(data)
+                    canInteract = function(_, distance)
+                        return distance < 2.5 and config.useTarget
                     end,
-                    canInteract = function()
-                        return not config.useTarget
-                    end
+                },
+            },
+
+            interact = {
+                distance = 3.0,
+                interactDst = 1.5,
+                options = {
+                    {
+                        label = 'Rent Vehicle',
+                        action = function()
+                            local data = { licenseType = 'pilot', menuType = 'aircraft' }
+                            checkLicenseData(data)
+                        end,
+                        canInteract = function()
+                            return not config.useTarget
+                        end
+                    }
                 }
             }
-        }
-    })
+        })
+    end
+
+    for i = 1, #boatcoords do
+        Renewed.addPed({
+            model = `mp_m_boatstaff_01`,                                      -- The ped to be spawned
+            dist = 25,                                                        -- Distance of when the ped should be loaded in
+            coords = vec3(boatcoords[i].x, boatcoords[i].y, boatcoords[i].z), -- The coords for the ped
+            heading = boatcoords[i].w,                                        -- Heading of the ped
+            scenario = 'WORLD_HUMAN_CLIPBOARD',                               -- Use this if you want the ped to play a scenario
+            freeze = true,                                                    -- use this if the ped should be frozen
+            invincible = true,                                                -- use this if the ped should be invincible
+            tempevents = true,                                                -- use this if the ped should block temporary events
+            id = 'rentals_boat_vehicles',                                     -- Unique ID for the ped
+
+            -- Normal ox Target stuff --
+            target = {
+                {
+                    name = 'rentals_air_vehicles',
+                    icon = "fas fa-car",
+                    label = "Rent Vehicle",
+                    onSelect = function()
+                        local data = { menuType = 'boat' }
+                        MPc.openVehicleMenu(data)
+                    end,
+                    canInteract = function(_, distance)
+                        return distance < 2.5 and config.useTarget
+                    end,
+                },
+            },
+
+            interact = {
+                distance = 3.0,
+                interactDst = 1.5,
+                options = {
+                    {
+                        label = 'Rent Vehicle',
+                        action = function()
+                            local data = { menuType = 'boat' }
+                            checkLicenseData(data)
+                        end,
+                        canInteract = function()
+                            return not config.useTarget
+                        end
+                    }
+                }
+            }
+        })
+    end
 end
 
 local function CreateRentalBlips()
     for _, blip in pairs(config.vehBlips) do
-        Utils.createBlips(blip.title, vec3(blip.coords.x, blip.coords.y, blip.coords.z), blip.id, blip.scale or 0.65, blip.color)
+        Utils.createBlips(blip.title, vec3(blip.coords.x, blip.coords.y, blip.coords.z), blip.id, blip.scale or 0.65,
+            blip.color)
     end
 end
 
